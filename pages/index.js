11afc5e685ddd59618750d0f87496e2cb98d7455
Paulovgtp <paulovgtp@gmail.com>
2024-04-03 04:13:39 +0000

    altered mensage of by initial page (add new paragraph)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Teste de exportação padrão de site em Porta Pública</h1>
+    return <h1>Oi oi, meu bem!</h1>;
+    return <h2>Tô correndo conta o tempo. Não estava preparado para essa nova etapa da nossa vida, mas serei seu porto seguro e com você consquistarei tudo que almeijo!</h2>;
+    return <h2>Peço a Deus que me ajude a realizar estes sonhos com você.</h2>;
 }
 
 export default Home;
